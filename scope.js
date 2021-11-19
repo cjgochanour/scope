3f12940cccddf5fe257145c8ef3005ea2e4e46c1
cjgochanour <cjgochanour@gmail.com>
2021-11-19 13:18:43 -0600

    Finished Mod Squad

--- scope.js
+++ scope.js
@@ -23,3 +23,30 @@ const conjunction = function (firstWord, secondWord) {
 
 let masterC = conjunction("Master", "Card");
 console.log(masterC);
+
+// Mod Squad
+
+const modFun = () => {
+    const modSquad = {
+        members: [
+            "Pete Cochran",
+            "Linc Hayes",
+            "Julie Barnes",
+            "Capt. Adam Greer",
+            "Chief Barney Metcalf",
+        ],
+        series: {
+            start: "1968",
+            end: "1973",
+        },
+    };
+
+    let HTMLRepresentation = `<h1>The Mod Squad</h1>`;
+
+    modSquad.members.forEach((member) => {
+        HTMLRepresentation += `<div>${member}</div>`;
+    });
+    return HTMLRepresentation;
+};
+
+console.log(modFun());
