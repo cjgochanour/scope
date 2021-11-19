d980a6498abd4b4cdbf5f551b158e78a1dc69d4b
cjgochanour <cjgochanour@gmail.com>
2021-11-19 13:07:18 -0600

    C is for Cookie Complete?

---
+++ scope.js
@@ -0,0 +1,13 @@
+const cookies = [
+    "Oatmeal Raisin",
+    "Chocolate Chip",
+    "Sugar",
+    "Peanut Butter",
+    "Snickerdoodle",
+    "Ginger",
+];
+
+for (let y = 0; y < cookies.length; y++) {
+    const currentCookie = cookies[y];
+    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`);
+}
